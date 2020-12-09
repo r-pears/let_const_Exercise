b97d04e4b6c1d86febc49ce509393e387abb92b2
Ryjen Pears <ryjen.pears@gmail.com>
2020-12-09 11:43:24 +0100

    let and const exercise

---
+++ exercise.js
@@ -0,0 +1,9 @@
+// ES2015 Global Constants
+const PI = 3.14;
+PI = 42; // Returns Error!
+
+// Quiz
+// 1. let can't be redeclared, and is block-scoped. var can be redeclared, and is function-scoped.
+// 2. const can't be reassigned, can't be redeclared, and is block-scoped. var can be redeclared, reassigned, and is function-scoped.
+// 3. let can be reassigned. const can not be reassigned.
+// 4. Hoisting only works for var, it declares all variables first, so they will return undefined if they are called before they are assigned.
